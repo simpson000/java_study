5ee42e623c0cbdd677c4a2ff9f5c053fd5ee2b5c
Sim yun jae <sim232811@naver.com>
2024-11-20 14:24:11 +0900

    2

---
+++ src/main/java/variable/ex/OperationEx1.java
@@ -0,0 +1,23 @@
+package variable.ex;
+
+public class OperationEx1 {
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        num1 = 10;
+        num2 = 20;
+        num3 = 30;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+        int avg = sum / 3;
+        System.out.println(avg);
+
+
+        double val1 = 1.5;
+        double val2 = 2.5;
+        double val3 = 3.5;
+
+
+        sum
+
+    }
+}
